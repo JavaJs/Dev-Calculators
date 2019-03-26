9cf1c7ad3cb6c5d101e48ab5dfb34fa4f49af59d
JavaJs <47489643+JavaJs@users.noreply.github.com>
2019-03-26 12:55:25 -0500

    Multiplication Files
    Here is the editable and runned files you can edit.

---
+++ Dev-Calc v1.0.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	std::cout << "Welcome to Multi-Calc v1.0!"<< std::endl;
+	std::cout << "Enter a your first number to multiply: "<< std::endl;
+    
+    int numinput1;
+    std::cin >> numinput1;
+    std::cout << "Okay enter your second number: " << std::endl;
+    int numinput2;
+    std::cin >> numinput2;
+    std::cout << "Here is your number: "<< std::endl;
+    std::cout << numinput1 * numinput2;
+    
+
+	
+  return 0;
+}
